bdfb5c3c8e4a6bcf59bdf3d1fc073a88f78a95f8
Jarek Jakubowski <jarjak@tsh.io>
2019-04-20 13:41:31 +0200

    Version 3.0

--- src/JarJak/DBALManagerBundle/DependencyInjection/JarJakDBALManagerExtension.php
+++ src/JarJak/DBALManagerBundle/DependencyInjection/JarJakDBALManagerExtension.php
@@ -20,8 +20,8 @@ class JarJakDBALManagerExtension extends Extension
     {
         $configuration = new Configuration();
         $this->processConfiguration($configuration, $configs);
-        
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
+
+        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.xml');
     }
 }
